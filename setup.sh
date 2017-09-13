143276403b5efe75bbd573934ad73c6a9c77c61c
Michiaki Ariga <ariga@cloudera.com>
2017-09-13 06:39:29 +0000

    Add constraints.txt

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 mkdir -p data
 wget https://snap.stanford.edu/data/movies.txt.gz -O data/movies.txt.gz
 hdfs dfs -mkdir movies
+pip install -r requirements.txt -c constraints.txt
 python movies.py | hdfs dfs -put - movies/movies.json
