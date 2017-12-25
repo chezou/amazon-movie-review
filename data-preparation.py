892243a36bdfc98a8faf5b56f1ad889f11e513c8
Michiaki Ariga <ariga@cloudera.com>
2017-12-25 07:25:28 +0000

    Save table of Amazon-meta

--- data-preparation.py
+++ data-preparation.py
@@ -9,7 +9,7 @@ spark = SparkSession\
     .appName("MovieDataPreparation")\
     .getOrCreate()
 
-## Original schema of json
+## Original schema of json for movies.json
 # ```
 # schema_json = StructType([
 #                    StructField("product/productId", StringType(), nullable=False),
@@ -45,5 +45,36 @@ df2.show()
 
 
 df2.write.saveAsTable('amazon_movie_reviews', format="parquet", mode='overwrite')
+
+## Original schema of json for meta.json
+# ```
+# schema_json = StructType([
+#                    StructField("ASIN", StringType(), nullable=False),
+#                    StructField("categories", ArrayType(StringType(), True), nullable=True),
+#                    StructField("group", StringType(), nullable=True),
+#                    StructField("reviews", ArrayType(), nullable=True),
+#                    StructField("salesrank", LongType(), nullable=True),
+#                    StructField("similar_items", ArrayType(LongType(), True), nullable=True),
+#                    StructField("title", StringType(), nullable=True),
+#                    ])
+# ```
+
+
+df_meta = spark.read.json("movies/meta.json")
+df_meta = df_meta.withColumn('id', df_meta['id'].cast("long"))
+df_meta = df_meta.withColumn('salesrank', df_meta['salesrank'].cast("long"))
+
+df3 = df_meta.selectExpr("ASIN as product_id",
+                         "categories",
+                         "group",
+                         "reviews",
+                         "salesrank as sales_rank",
+                         "similar_items",
+                         "title"
+                        )
+
+df3.show()
+
+df3.write.saveAsTable('amazon_meta', format="parquet", mode="overwrite")
 
 spark.stop()
