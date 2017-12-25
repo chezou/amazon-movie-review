8493420aa044483bb7ac5f07bd9374961fe855ac
Michiaki Ariga <ariga@cloudera.com>
2017-12-25 07:26:21 +0000

    Add movie title analysis

--- data-visualization.py
+++ data-visualization.py
@@ -9,6 +9,9 @@ spark = SparkSession\
     .getOrCreate()
 
 df = spark.table("amazon_movie_reviews")
+meta = spark.table("amazon_meta").select("product_id", "title")
+df = df.join(meta, "product_id")
+
 df.dropna().groupBy("score").count().sort("score").show()
 pd_df = df.groupBy("score", "helpfulness_agreed").count().toPandas()
 
@@ -16,7 +19,7 @@ pd_df = df.groupBy("score", "helpfulness_agreed").count().toPandas()
 pd_df.dropna().groupby(['helpfulness_agreed'])[['count']].sum().plot(logy=True)
 
 ### Frequency of `score`
-pd_df.dropna().groupby(['score'])[['count']].sum().plot()
+pd_df.dropna().groupby(['score'])[['count']].sum().plot(kind='bar')
 
 ### Relationship beween score and helpfulness_agreed
 import seaborn as sns
@@ -24,7 +27,7 @@ sns.jointplot(x="score", y="helpfulness_agreed", kind='kde',data=pd_df)
 
 ### Summarize reviewed year and score
 
-pdf = df.dropna().select("product_id","score", year('reviewed_at').alias('year')).groupBy("score", "year").count().toPandas()
+pdf = df.dropna().select("product_id","score", "title", year('reviewed_at').alias('year')).groupBy("score", "year").count().toPandas()
 pdf.plot.scatter(x='year', y='score', s=pdf['count']*0.002)
 
 #sns.heatmap(pdf.pivot('year','score', 'count'))
@@ -33,7 +36,9 @@ pdf.pivot('year','score', 'count').plot(kind='bar', stacked=True)
 ### Show top reviewd 10 users
 df.groupby('user_id').count().sort(desc('count')).show(10)
 
-### Show top reviewd movies
-df.dropna().groupby("product_id").count().sort(desc('count')).show(10)
+### Show top reviewed movies
+df.dropna().groupby("product_id", "title").count().sort(desc('count')).show(10)
+
+# It appears that there are same movies but having different ids
 
 spark.stop()
